7ad8250c25acbc3b4489c64a1ba027f8f74c4da4
Pablo de la Peña <p@shedcollective.org>
2015-10-17 13:57:40 +0100

    Updated services to allow for easy app overriding

--- services/services.php
+++ services/services.php
@@ -4,6 +4,11 @@ return array(
     'models' => array(
         'Faq' => function () {
             return new \Nails\Faq\Model\Faq();
+            if (class_exists('\App\Faq\Model\Faq')) {
+                return new \App\Faq\Model\Faq();
+            } else {
+                return new \Nails\Faq\Model\Faq();
+            }
         }
     )
 );
