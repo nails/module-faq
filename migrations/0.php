45336a0760a3bc47f1648cdf6e602e061fb2d4e9
Pablo de la Peña <p@shedcollective.org>
2017-01-29 21:26:29 +0000

    Converted SQL migrations into PHP migrations

---
+++ migrations/0.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Migration:   0
+ * Started:     26/04/2015
+ * Finalised:   26/04/2015
+ */
+
+namespace Nails\Database\Migration\Nailsapp\ModuleFaq;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration0 extends Base
+{
+    /**
+     * Execute the migration
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query("
+            CREATE TABLE `{{NAILS_DB_PREFIX}}faq` (
+                `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                `label` varchar(150) DEFAULT NULL,
+                `group` varchar(150) DEFAULT NULL,
+                `body` text,
+                `order` int(11) unsigned NOT NULL DEFAULT '0',
+                `isActive` tinyint(1) unsigned NOT NULL DEFAULT '1',
+                `created` datetime NOT NULL,
+                `created_by` int(11) unsigned DEFAULT NULL,
+                `modified` datetime NOT NULL,
+                `modified_by` int(11) unsigned DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                KEY `created_by` (`created_by`),
+                KEY `modified_by` (`modified_by`),
+                CONSTRAINT `{{NAILS_DB_PREFIX}}faq_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `{{NAILS_DB_PREFIX}}user` (`id`) ON DELETE SET NULL,
+                CONSTRAINT `{{NAILS_DB_PREFIX}}faq_ibfk_2` FOREIGN KEY (`modified_by`) REFERENCES `{{NAILS_DB_PREFIX}}user` (`id`) ON DELETE SET NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
+    }
+}
--- migrations/0.sql
+++
@@ -1,4 +0,0 @@
-# INITIAL ADMIN DB
-# This is the schema of the faq module database as of 26/04/2015
-DROP TABLE IF EXISTS `{{NAILS_DB_PREFIX}}faq`;
-CREATE TABLE `nails_faq` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`label` varchar(150) DEFAULT NULL,`group` varchar(150) DEFAULT NULL,`body` text,`order` int(11) unsigned NOT NULL DEFAULT '0',`isActive` tinyint(1) unsigned NOT NULL DEFAULT '1',`created` datetime NOT NULL,`created_by` int(11) unsigned DEFAULT NULL,`modified` datetime NOT NULL,`modified_by` int(11) unsigned DEFAULT NULL,PRIMARY KEY (`id`),KEY `created_by` (`created_by`),KEY `modified_by` (`modified_by`),CONSTRAINT `{{NAILS_DB_PREFIX}}faq_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `{{NAILS_DB_PREFIX}}user` (`id`) ON DELETE SET NULL,CONSTRAINT `{{NAILS_DB_PREFIX}}faq_ibfk_2` FOREIGN KEY (`modified_by`) REFERENCES `{{NAILS_DB_PREFIX}}user` (`id`) ON DELETE SET NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
