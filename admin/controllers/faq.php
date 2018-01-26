3d9070071b5b58bebd2dbd3ef6f7fb311378bb96
Pablo de la Peña <p@shedcollective.org>
2018-01-26 14:25:35 +0000

    Minor aesthetic correction in admin

--- admin/controllers/faq.php
+++ admin/controllers/faq.php
@@ -19,4 +19,6 @@ class Faq extends DefaultController
     const CONFIG_MODEL_PROVIDER = 'nailsapp/module-faq';
     const CONFIG_SIDEBAR_GROUP  = 'FAQs';
     const CONFIG_SIDEBAR_ICON   = 'fa-question-circle';
+    const CONFIG_TITLE_SINGLE   = 'FAQ';
+    const CONFIG_TITLE_PLURAL   = 'FAQs';
 }
